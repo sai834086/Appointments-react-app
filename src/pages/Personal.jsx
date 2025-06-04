914121a9f1ff45a06dbac8387dd22f41a999de7f
Sai Kumar <saikumarj555@gmail.com>
2025-06-04 12:42:21 -0500

    first commit

--- src/pages/Personal.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Personal.module.css"
-
-export default function Personal() {
-  return (
-    <div className={styles.personal}>
-      <h2>Love You Kanna 😘😘💕</h2>
-    </div>
-  );
-}
